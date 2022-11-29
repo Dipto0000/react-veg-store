3245c84ca352236db74f88dd8c4afb87a3ed3cb8
Shahriar Ahmed <dipto2041@gmail.com>
2022-11-29 23:28:19 +0600

    finally done with a rush

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Products from "./Components/Products/Products";
+import AssignmentQuestion from "./Components/AssignmentQuestion/AssignmentQuestion";
 
 function App() {
   return (
     <div className="App">
       <Products></Products>
+      <AssignmentQuestion></AssignmentQuestion>
     </div>
   );
 }
--- src/Components/AssignmentQuestion/AssignmentQuestion.js
+++ src/Components/AssignmentQuestion/AssignmentQuestion.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const AssignmentQuestion = () => {
-    return (
-        <div>
-            <p>
-                <strong>Q1: How react works?</strong> 
-
-                ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code. It creates a virtual dom whenever something is changed and re render by comparing with the virtual dom. Then the changed items are shown in the UI</p>
-
-                <strong>Q2: How useState works?</strong>
-
-                UseState encapsulate only singular value from the state, for multiple state need to have useState calls. Syntax: The first element is the initial state and the second one is a function that is used for updating the state. We can also pass a function as an argument if the initial state has to be computed.
-        </div>
-    );
+  return (
+    <div>
+      <p>
+        <strong>Q1: How react works?</strong>
+        ReactJS divides the UI into isolated reusable pieces of code known as
+        components. React components work similarly to JavaScript functions as
+        they accept arbitrary inputs called properties or props. It's possible
+        to have as many components as necessary without cluttering your code. It
+        creates a virtual dom whenever something is changed and re render by
+        comparing with the virtual dom. Then the changed items are shown in the
+        UI
+      </p>
+      <strong>Q2: How useState works?</strong>
+      UseState encapsulate only singular value from the state, for multiple
+      state need to have useState calls. Syntax: The first element is the
+      initial state and the second one is a function that is used for updating
+      the state. We can also pass a function as an argument if the initial state
+      has to be computed.
+    </div>
+  );
 };
 
 export default AssignmentQuestion;
